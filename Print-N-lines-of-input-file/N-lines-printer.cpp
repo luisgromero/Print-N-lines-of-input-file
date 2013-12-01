35638d594d1e8073835b7a5792aef212a5a41fd8
Luis G Romero <luisgromero@live.com>
2013-11-30 20:51:31 -0800

    Added stack structure to save lines of text and read them from last to first.

--- Print-N-lines-of-input-file/N-lines-printer.cpp
+++ Print-N-lines-of-input-file/N-lines-printer.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
-#include <algorithm>
 #include <string>
+#include <stack>
 /*
 Program that reads last N lines from a text file.
 */
@@ -10,18 +10,37 @@ using namespace std;
 
 int main(int argc, char *argv[]){
 	ifstream in_stream;
+	string line;
+	stack<string>lines;
+	int n;
+
 	cout << argv[1] << endl << endl;
 	in_stream.open(argv[1]);
-	string line;
-	if (!in_stream.is_open())
-	{
+	
+
+	if (!in_stream.is_open()){	
 		cerr << "Cannot open the file." << endl;
 	}
-	while (!in_stream.eof())
-	{
+
+	while (!in_stream.eof()){
 		in_stream >> line;
-		cout << line;
+		lines.push(line);
+		//cout << line;
 	}
+	cout << "done adding to stack" << endl;
+	cout << "Number of lines available to print: " << lines.size()<<endl;
+	cout << "How many lines from end of begining you want to print to screen: ";
+	cin >> n;
+
+	cout <<endl<< "printing N: " << n << "lines from file";
+	
+	while (n>0 && !lines.empty())
+	{
+		line = lines.top();
+		lines.pop();
+		cout << line << endl;
+	}
+
 	cout << endl << endl;
 	system("pause");
 	return 0;
